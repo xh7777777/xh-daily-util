7aec43518ce2c3d33b03e818fbb368b2075d5124
XIAOHUAN <490327763@qq.com>
2024-08-07 17:56:52 +0800

    feat: first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="index">
+    <Counter />
+    <view>距离下班还有</view>
+  </view>
+</template>
+
+<script>
+import './index.scss'
+import Counter from '../../components/Counter.vue'
+
+export default {
+  name: 'Index',
+  components: {
+    Counter
+  }
+}
+</script>
